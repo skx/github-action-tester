9b95b0274d0bc411efd8aa31cce4b6e0f26b87c2
Steve Kemp <steve@steve.org.uk>
2019-02-24 09:22:04 +0200

    Updated comments

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 set -e
 
+#
+# If the script is present ..
+#
 if [ -e .github/run-tests.sh ]; then
+
+    # Ensure it is executable.
     chmod 755 .github/run-tests.sh
+
+    # Run it.
     .github/run-tests.sh
+
+    # Preserve the exit-code
+    exit $?
+
 else
+
+    #
+    # Script is missing.
+    #
     echo "Repository does not contain test-script '.github/run-tests.sh'"
     exit 1
 fi
