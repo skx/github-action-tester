33992017518e388aa4ab5865133ba78786862e67
Steve Kemp <steve@steve.org.uk>
2019-09-07 18:27:49 +0300

    Simplify the logic and enhance the comments.
    The comments were updated for clarity, and the useless `exit $?`
    was removed.  This closes #5.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+#
+# Terminate upon errors
+#
 set -e
 
 #
@@ -6,20 +10,38 @@ set -e
 #
 if [ -e .github/run-tests.sh ]; then
 
+    #
     # Ensure it is executable.
+    #
     chmod 755 .github/run-tests.sh
 
+    #
     # Run it.
+    #
     .github/run-tests.sh
 
-    # Preserve the exit-code
-    exit $?
-
-else
-
     #
-    # Script is missing.
+    # If the script exits with a non-zero status-code
+    # then it will terminate this script due to the use
+    # of `set -e`.
     #
-    echo "Repository does not contain test-script '.github/run-tests.sh'"
-    exit 1
+    # So when we reach this point we know:
+    #
+    #  1.  We've executed the test-script.
+    #
+    #  2.  The test-script passed.
+    #
+    # Exit cleanly because we're done.
+    #
+    exit 0
 fi
+
+
+#
+# There is no test-script within the repository.
+#
+# Show that, and exit with a failure-code.
+#
+echo "The repository does not contain a test-script '.github/run-tests.sh'"
+exit 1
+
